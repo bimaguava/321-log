a0f2e83ce8bb809e380b1341e0208c260526db28
Bima <bimaguava@gmail.com>
2025-01-19 02:19:36 +0700

    media: use cloudinary

--- tina/config.js
+++ tina/config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "tinacms";
-
 import { blog_postFields } from "./templates";
 import { titleFields } from "./templates";
+import {
+  mediaHandlerConfig,
+  createMediaHandler,
+} from 'next-tinacms-cloudinary/dist/handlers';
+import { isAuthorized } from '@tinacms/auth';
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
@@ -10,12 +14,31 @@ const branch =
   process.env.HEAD ||
   "main";
 
+// Cloudinary media handler configuration
+const mediaHandler = createMediaHandler({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  authorized: async (req, _res) => {
+    try {
+      if (process.env.NODE_ENV === 'development') {
+        return true;
+      }
+
+      const user = await isAuthorized(req);
+      return user && user.verified;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  },
+});
+
 export default defineConfig({
   branch,
 
   // Get this from tina.io
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
   token: process.env.TINA_TOKEN,
 
   client: { skip: true },
@@ -24,9 +47,8 @@ export default defineConfig({
     publicFolder: "public",
   },
   media: {
-    tina: {
-      mediaRoot: "images",
-      publicFolder: "public",
+    loadCustomStore: async () => {
+      return mediaHandler; // Use the Cloudinary media handler
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
