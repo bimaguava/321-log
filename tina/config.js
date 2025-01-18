ae59864511bf8375e3a565e1dfe53f3258842579
Bima <bimaguava@gmail.com>
2025-01-19 01:28:16 +0700

    media

--- tina/config.js
+++ tina/config.js
@@ -25,8 +25,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "images",
-      publicFolder: "public/images",
+      mediaRoot: "/images",
+      publicFolder: "/public",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
